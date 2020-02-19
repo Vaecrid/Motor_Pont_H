57ce765c5011539992c3f1bfaaf00e154f8ac2e7
Vaecrid <57478439+Vaecrid@users.noreply.github.com>
2020-02-19 20:11:55 +0100

    Add files via upload

---
+++ Motor_Pont_H.ino
@@ -0,0 +1,20 @@
+const int pinENA = 6;
+const int pinIN1 = 7;
+const int pinIN2 = 8;
+ 
+const int speed = 200;      //velocidad de giro 80% (200/255)
+ 
+void setup()
+{
+   pinMode(pinIN1, OUTPUT);
+   pinMode(pinIN2, OUTPUT);
+   pinMode(pinENA, OUTPUT);
+}
+ 
+void loop()
+{
+   digitalWrite(pinIN1, HIGH);
+   digitalWrite(pinIN2, LOW);
+   analogWrite(pinENA, speed);
+   delay(1000);
+}
